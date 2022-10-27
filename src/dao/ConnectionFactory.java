2f4ed5592ab58732bc72d5e3c7087f6ba3197d9f
Wisllys Lima <wisllyslima96@gmail.com>
2022-10-27 11:38:27 -0300

    Vai tudo!

---
+++ src/dao/ConnectionFactory.java
@@ -0,0 +1,22 @@
+package dao;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    Connection conexao;
+
+    public Connection getConexao() {
+        try {
+            conexao = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/crudescola", "root", "");
+            /*if (conexao != null) {
+                System.out.println("Conexao estabelecida com o banco de dados!!");
+            }*/
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return conexao;
+    }
+}
